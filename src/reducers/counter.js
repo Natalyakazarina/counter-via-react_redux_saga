8a0545540eb7c0e49572455999034bb6748c7e87
Natalyakazarina <natalya.kazarina@list.ru>
2020-05-20 16:50:20 +0300

    some amedments to reducers

--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -5,41 +5,40 @@ const initialState = {
   fetchCounterError: "",
 };
 
-let counterValue = initialState.counters.value;
-
 function counterReducer(state = initialState, action) {
   switch (action.type) {
     case 'COUNTER/FETCH_COUNTERS_ADD':
-      let addValue = counterValue + 1;
-
-      return update(state, {
-        counters: {
-          $push: [
-            {
-              id: state.id,
-              value: addValue,
-            },
-          ],
-        },
+      const changeAddState = state.counters.slice();
+      let indexToUpdate;
+     changeAddState.forEach((counter, index, value) => {
+        if (counter.id === action.payload.id) {
+          indexToUpdate = index;
+          changeAddState[indexToUpdate].value=state.counters[indexToUpdate].value + 1
+        }
       });
+      return update(state, {
+        $merge:
+          {
+            counters: changeAddState,
+          }
+        });
     case 'COUNTER/FETCH_COUNTERS_DEDUCT':
-      let deductValue;
-      if (counterValue <= 0) {
-        deductValue = 0;
-      } else {
-        deductValue = counterValue - 1;
-      }
-
-      return update(state, {
-        counters: {
-          $push: [
-            {
-              id: state.id,
-              value: deductValue,
-            },
-          ],
-        },
+      
+      const changeDeductState = state.counters.slice();
+      let indexUpdate;
+     changeDeductState.forEach((counter, index, value) => {
+        if (counter.id === action.payload.id) {
+          indexUpdate = index;
+          changeDeductState[indexUpdate].value=state.counters[indexUpdate].value - 1
+        }
       });
+      return update(state, {
+        $merge:
+          {
+            counters: changeDeductState,
+          }
+        });
+  
     case 'COUNTER/FETCH_COUNTERS_ERROR':
       return update(state, {
         $merge: {
